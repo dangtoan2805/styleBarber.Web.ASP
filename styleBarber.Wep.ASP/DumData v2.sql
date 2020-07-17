06bef6f718122cd6021a44ee52f11f58241f7d1e
ToanDang <dtoan2805@gmail.com>
2020-07-17 15:12:39 +0700

    big update v2

---
+++ styleBarber.Wep.ASP/DumData v2.sql
@@ -0,0 +1,89 @@
+INSERT INTO Barbers
+        ([Image],[isFounder],[Name],[Info],[LinkFB])
+VALUES('barber_01.jpg', '1', 'Abel Franco', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com'),
+        ('barber_02.jpg', '1', 'Camden B. Brock', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com'),
+        ('barber_03.jpg', '0', 'Griffith T. Collins', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com'),
+        ('barber_05.jpg', '0', 'Flynn M. Higgins', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com'),
+        ('barber_05.jpg', '0', 'Harding Leblanc', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com'),
+        ('barber_03.jpg', '0', 'Shafira Hughes', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com'),
+        ('barber_02.jpg', '0', 'Isaiah F. Thornton', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com'),
+        ('barber_03.jpg', '0', 'Cynthia Sears', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com'),
+        ('barber_03.jpg', '0', 'Dale W. Mcleod', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com'),
+        ('barber_05.jpg', '0', 'Kaden Odom', N'Có truyền thống làm nghề lâu đời, cha của hai anh là một người lão luyện trong nghề đã hơn 40 năm, nhưng đam mê của cha vẫn chưa bao giờ dừng lại, dù tuổi đã cao, nhưng cha vẫn thầm lặng đứng dưới góc me che bóng râm phủ ngang đầu', 'fb.com');
+
+INSERT INTO StyleHairs
+        ([Image],[Title],[StyleDescription])
+VALUES('style_01.jpg', N'Kiểu tóc 01', N'Những bạn nữ sở hữu gương mặt tròn thường bị hạn chế trong việc tìm ra kiểu tóc phù hợp với gương mặt của mình. '),
+        ('style_02.jpg', N'Kiểu tóc 02', N'Những bạn nữ sở hữu gương mặt tròn thường bị hạn chế trong việc tìm ra kiểu tóc phù hợp với gương mặt của mình. '),
+        ('style_03.jpg', N'Kiểu tóc 03', N'Những bạn nữ sở hữu gương mặt tròn thường bị hạn chế trong việc tìm ra kiểu tóc phù hợp với gương mặt của mình. '),
+        ('style_04.jpg', N'Kiểu tóc 04', N'Những bạn nữ sở hữu gương mặt tròn thường bị hạn chế trong việc tìm ra kiểu tóc phù hợp với gương mặt của mình. '),
+        ('style_05.jpg', N'Kiểu tóc 05', N'Những bạn nữ sở hữu gương mặt tròn thường bị hạn chế trong việc tìm ra kiểu tóc phù hợp với gương mặt của mình. '),
+        ('style_06.jpg', N'Kiểu tóc 06', N'Những bạn nữ sở hữu gương mặt tròn thường bị hạn chế trong việc tìm ra kiểu tóc phù hợp với gương mặt của mình. '),
+        ('style_07.jpg', N'Kiểu tóc  07', N'Những bạn nữ sở hữu gương mặt tròn thường bị hạn chế trong việc tìm ra kiểu tóc phù hợp với gương mặt của mình. ')
+
+INSERT INTO Services
+        ([Image],[Name],[ServiceDescription],[Price])
+VALUES('service_01.jpg', N'Rữa mặt', N'BarberShop Vũ Trí – sự kết hợp giữa tên của hai anh em ruột thịt, anh Đỗ Linh Vũ và anh Trí Tam Quốc.', 30000),
+        ('service_02.jpg', N'Cắt tóc', N'BarberShop Vũ Trí – sự kết hợp giữa tên của hai anh em ruột thịt, anh Đỗ Linh Vũ và anh Trí Tam Quốc.', 70000),
+        ('service_03.jpg', N'Rày tai', N'BarberShop Vũ Trí – sự kết hợp giữa tên của hai anh em ruột thịt, anh Đỗ Linh Vũ và anh Trí Tam Quốc.', 30000),
+        ('service_04.jpg', N'Tattoo', N'BarberShop Vũ Trí – sự kết hợp giữa tên của hai anh em ruột thịt, anh Đỗ Linh Vũ và anh Trí Tam Quốc.', 50000),
+        ('service_05.jpg', N'Gọi đầu', N'BarberShop Vũ Trí – sự kết hợp giữa tên của hai anh em ruột thịt, anh Đỗ Linh Vũ và anh Trí Tam Quốc.', 50000),
+        ('service_06.jpg', N'Cạo râu', N'BarberShop Vũ Trí – sự kết hợp giữa tên của hai anh em ruột thịt, anh Đỗ Linh Vũ và anh Trí Tam Quốc.', 10000)
+
+INSERT INTO Reviewers
+        ([Image],[Name],[Job],[Review])
+VALUES('avatar_01.png', 'Octavius Page', 'Quam Elementum Associates', N'Ngoài chuỗi hệ thống cửa hàng cắt tóc nam, Barbershop Vũ Trí còn phát triển thêm mảng đào tạo thợ chuyên nghiệp về cắt tóc nam lấy tên là  Vũ Trí Academy dưới sự giảng dạy của thầy Tuấn Anh Barber'),
+        ('avatar_02.png', 'Elizabeth B. Barnett', 'Fermentum Vel Institute', N'Ngoài chuỗi hệ thống cửa hàng cắt tóc nam, Barbershop Vũ Trí còn phát triển thêm mảng đào tạo thợ chuyên nghiệp về cắt tóc nam lấy tên là  Vũ Trí Academy dưới sự giảng dạy của thầy Tuấn Anh Barber'),
+        ('avatar_02.png', 'Yeo Huber', 'Ante Vivamus Corporation', N'Ngoài chuỗi hệ thống cửa hàng cắt tóc nam, Barbershop Vũ Trí còn phát triển thêm mảng đào tạo thợ chuyên nghiệp về cắt tóc nam lấy tên là  Vũ Trí Academy dưới sự giảng dạy của thầy Tuấn Anh Barber'),
+        ('avatar_01.png', 'Dexter Rosales', 'Ac Nulla In Limited', N'Ngoài chuỗi hệ thống cửa hàng cắt tóc nam, Barbershop Vũ Trí còn phát triển thêm mảng đào tạo thợ chuyên nghiệp về cắt tóc nam lấy tên là  Vũ Trí Academy dưới sự giảng dạy của thầy Tuấn Anh Barber'),
+        ('avatar_01.png', 'Stacy Preston', 'Orci LLC', N'Ngoài chuỗi hệ thống cửa hàng cắt tóc nam, Barbershop Vũ Trí còn phát triển thêm mảng đào tạo thợ chuyên nghiệp về cắt tóc nam lấy tên là  Vũ Trí Academy dưới sự giảng dạy của thầy Tuấn Anh Barber'),
+        ('avatar_02.png', 'Karen L. Patterson', 'Dis Parturient Associates', N'Ngoài chuỗi hệ thống cửa hàng cắt tóc nam, Barbershop Vũ Trí còn phát triển thêm mảng đào tạo thợ chuyên nghiệp về cắt tóc nam lấy tên là  Vũ Trí Academy dưới sự giảng dạy của thầy Tuấn Anh Barber'),
+        ('avatar_01.png', 'Galvin Perry', 'A Company', N'Ngoài chuỗi hệ thống cửa hàng cắt tóc nam, Barbershop Vũ Trí còn phát triển thêm mảng đào tạo thợ chuyên nghiệp về cắt tóc nam lấy tên là  Vũ Trí Academy dưới sự giảng dạy của thầy Tuấn Anh Barber')
+
+INSERT INTO InfoStores
+        ([Logo],[About],[Mission],[Reason])
+VALUES
+        ('logo-mini.png',
+                N'BarberShop Vũ Trí chuyên Nhuộm tóc nam, Dao tao cat toc nam, Hớt tóc nam, Tattoo hair,….– Với 25 năm kinh nghiệm trong nghề Bạn sẽ trải nghiệm được dịch vụ tốt nhất của chúng tôi. Chúng tôi tự tin là sự lựa chọn hàng đầu cho bạn.',
+                N'Khóa đào tạo cơ bản và nâng cao kéo dài 6 tháng với học phí là 1000$. Cam kết ra nghề 100% Học viên được học cắt tóc giả trên đầu manơcanh và mỗi 2 tuần 1 lần học viên được cắt tóc thật cho trẻ em, trại mồ côi, trường tiểu học,… Học viên ở xa có thể ở lại kí túc xá của Barbershop Vũ Trí',
+                N'Với 25 năm kinh nghiệm trong nghề Bạn sẽ trải nghiệm được dịch vụ tốt nhất của chúng tôi. Chúng tôi tự tin là sự lựa chọn hàng đầu cho bạn. BarberShop Vũ Trí chuyên Nhuộm tóc nam, Dao tao cat toc nam, Hớt tóc nam, Tattoo hair,….– Với 25 năm kinh nghiệm trong nghề Bạn sẽ trải nghiệm được dịch vụ tốt nhất của chúng tôi.');
+
+
+INSERT INTO Contacts
+        ([FirstName],[LastName],[Email],[Phone],[Note])
+VALUES('Holmes', 'Hart', 'Cras.vehicula@enim.net', '974-976-2347', 'tellus faucibus leo, in lobortis tellus justo sit amet nulla.'),
+        ('George', 'Maxwell', 'sed.hendrerit.a@eleifend.co.uk', '978-729-8433', 'metus. In lorem. Donec elementum, lorem ut aliquam iaculis, lacus'),
+        ('Brian', 'Lopez', 'tellus.Nunc.lectus@Aliquamornarelibero.ca', '551-676-8993', 'Proin non massa non ante bibendum ullamcorper. Duis cursus, diam'),
+        ('Jesse', 'Webb', 'et.euismod@nequeNullam.net', '145-858-7462', 'tristique pellentesque, tellus sem mollis dui, in sodales elit erat'),
+        ('Jelani', 'Sharpe', 'Quisque.purus@augue.com', '951-753-3611', 'diam at pretium aliquet, metus urna convallis erat, eget tincidunt'),
+        ('Palmer', 'Frazier', 'ipsum@scelerisquedui.org', '631-108-1290', 'mi, ac mattis velit justo nec ante. Maecenas mi felis,'),
+        ('Graiden', 'Stephens', 'consequat.enim.diam@Aliquamnec.org', '998-487-8214', 'consequat enim diam vel arcu. Curabitur ut odio vel est'),
+        ('Mohammad', 'Peters', 'gravida.Praesent.eu@faucibus.com', '511-780-5245', 'diam at pretium aliquet, metus urna convallis erat, eget tincidunt'),
+        ('Avram', 'Daugherty', 'purus.Duis.elementum@vulputate.edu', '476-022-8945', 'Suspendisse sed dolor. Fusce mi lorem, vehicula et, rutrum eu,'),
+        ('Keegan', 'Morrison', 'ridiculus@dictum.com', '192-179-1445', 'laoreet, libero et tristique pellentesque, tellus sem mollis dui, in');
+
+
+INSERT INTO Appointments
+        ([FirstName],[LastName],[Email],[Phone],[Note],[Date],[Status],[BarberID])
+VALUES('Walker', 'Hall', 'diam@vitae.edu', '099-797-9960', 'vitae, erat. Vivamus nisi. Mauris nulla. Integer urna. Vivamus molestie', '07/08/2020', '0', 3),
+        ('Hammett', 'Hill', 'sit@et.ca', '752-484-0321', 'Duis risus odio, auctor vitae, aliquet nec, imperdiet nec, leo.', '05/05/2020', '0', 10),
+        ('Travis', 'Camacho', 'Suspendisse.ac.metus@ligulaNullamenim.com', '419-682-1057', 'lorem, luctus ut, pellentesque eget, dictum placerat, augue. Sed molestie.', '09/05/2019', '0', 9),
+        ('Jarrod', 'Bush', 'eget.dictum@placerat.ca', '051-547-2632', 'sit amet, dapibus id, blandit at, nisi. Cum sociis natoque', '06/16/2020', '1', 6),
+        ('Nasim', 'Hooper', 'ut.erat@veliteu.co.uk', '479-291-8060', 'tellus. Aenean egestas hendrerit neque. In ornare sagittis felis. Donec', '06/11/2020', '0', 2),
+        ('Eaton', 'Willis', 'non.ante@vel.com', '697-760-9891', 'mattis velit justo nec ante. Maecenas mi felis, adipiscing fringilla,', '05/17/2021', '0', 7),
+        ('Valentine', 'Oneill', 'hendrerit@erat.org', '604-689-3336', 'vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae', '12/14/2019', '1', 10),
+        ('Abbot', 'Kirk', 'egestas@dui.edu', '584-808-0042', 'urna. Nullam lobortis quam a felis ullamcorper viverra. Maecenas iaculis', '02/27/2020', '0', 2),
+        ('Hunter', 'Crosby', 'Aenean.eget@diamPellentesquehabitant.ca', '324-169-2867', 'ridiculus mus. Proin vel nisl. Quisque fringilla euismod enim. Etiam', '08/08/2020', '1', 7),
+        ('Wylie', 'Gould', 'libero.lacus@etnetus.com', '773-780-0047', 'sed, est. Nunc laoreet lectus quis massa. Mauris vestibulum, neque', '11/28/2020', '1', 10);
+INSERT INTO Appointments
+        ([FirstName],[LastName],[Email],[Phone],[Note],[Date],[Status],[BarberID])
+VALUES('Emmanuel', 'Davis', 'nec.cursus@urna.edu', '771-029-8595', 'tincidunt, nunc ac mattis ornare, lectus ante dictum mi, ac', '05/03/2021', '1', 6),
+        ('Tobias', 'Turner', 'nisi.nibh@lorem.com', '290-059-2586', 'Fusce fermentum fermentum arcu. Vestibulum ante ipsum primis in faucibus', '08/12/2019', '1', 10),
+        ('Steven', 'Wong', 'enim.diam@fames.net', '081-248-5445', 'sed, sapien. Nunc pulvinar arcu et pede. Nunc sed orci', '06/07/2020', '1', 6),
+        ('Jonas', 'Vaughn', 'Sed.dictum@In.com', '709-816-9970', 'Nullam feugiat placerat velit. Quisque varius. Nam porttitor scelerisque neque.', '10/05/2020', '1', 6),
+        ('Gary', 'Boyer', 'Etiam@enim.edu', '563-556-7258', 'dolor dapibus gravida. Aliquam tincidunt, nunc ac mattis ornare, lectus', '12/28/2020', '0', 8),
+        ('Stone', 'Baker', 'eros@ipsum.com', '643-491-2806', 'eleifend non, dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc', '12/21/2019', '0', 3),
+        ('Ulric', 'Jimenez', 'dolor.egestas@at.edu', '049-743-1943', 'sem mollis dui, in sodales elit erat vitae risus. Duis', '11/12/2019', '1', 9),
+        ('Raymond', 'Cummings', 'dictum@Maurisquisturpis.net', '443-831-4522', 'feugiat nec, diam. Duis mi enim, condimentum eget, volutpat ornare,', '04/20/2020', '0', 5),
+        ('Brenden', 'Mcclain', 'nunc.interdum.feugiat@nonummy.com', '530-162-5084', 'feugiat. Sed nec metus facilisis lorem tristique aliquet. Phasellus fermentum', '05/01/2020', '1', 6),
+        ('Bradley', 'Hart', 'justo.Praesent@justo.net', '449-433-5632', 'venenatis a, magna. Lorem ipsum dolor sit amet, consectetuer adipiscing', '11/11/2020', '1', 3);
